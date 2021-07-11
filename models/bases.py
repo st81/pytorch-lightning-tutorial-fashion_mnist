23f9492e08fe34132677112d3560ceedfde5a667
Shuto Takahahsi <m59rt@yahoo.co.jp>
2021-07-12 05:30:28 +0900

    [Modify] Compute validation accuracy by torchmetrics

--- models/bases.py
+++ models/bases.py
@@ -7,7 +7,7 @@ import torchmetrics
 
 class LitModel(pl.LightningModule):
 
-    def __init__(self, batch_size: int = 64):
+    def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
@@ -18,8 +18,8 @@ class LitModel(pl.LightningModule):
             nn.Linear(512, 10),
             nn.ReLU(),
         )
-        self.batch_size = batch_size
-        self.accuracy = torchmetrics.Accuracy()
+        self.train_acc = torchmetrics.Accuracy()
+        self.val_acc = torchmetrics.Accuracy()
 
     def forward(self, x):
         x = self.flatten(x)
@@ -36,22 +36,22 @@ class LitModel(pl.LightningModule):
         y_hat = self.linear_relu_stack(x)
         loss = F.cross_entropy(y_hat, y)
         self.log('Loss/train', loss, on_step=False, on_epoch=True)
-        # correct = (y_hat.argmax(1) == y).type(torch.float).sum().item()
-        # self.log('Accuracy/train', correct, on_step=False, on_epoch=True)
-        self.accuracy(y_hat, y)
+        self.train_acc(y_hat, y)
         return loss
 
     def training_epoch_end(self, outs):
-        self.log('Accuracy/train', self.accuracy.compute())
+        self.log('Accuracy/train', self.train_acc.compute())
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
         val_loss = F.cross_entropy(y_hat, y)
         self.log('Loss/val', val_loss, on_step=False, on_epoch=True)
-        correct = (y_hat.argmax(1) == y).type(torch.float).sum().item()
-        self.log('Accuracy/val', correct, on_step=False, on_epoch=True)
+        self.val_acc(y_hat, y)
         return val_loss
+    
+    def validation_epoch_end(self, outs):
+        self.log('Accuracy/val', self.val_acc.compute())
 
     def test_step(self, batch, batch_idx):
         x, y = batch
